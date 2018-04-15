7feed68960e69608c1fa4731b70618ae50c74b80
rithesh-gopal <rithesh.gopal@yahoo.com>
2018-04-15 12:33:50 +0530

    Added constants file

---
+++ src/org/ritheshgopal/httpserver/util/HttpConstants.java
@@ -0,0 +1,10 @@
+package org.ritheshgopal.httpserver.util;
+
+public class HttpConstants {
+
+	public static final String METHOD_NOT_ALLOWED="Method not allowed";
+	public static final int METHOD_NOT_ALLOWED_CODE = 405;
+	
+	public static final String NOT_FOUND="Not Found";
+	public static final int NOT_FOUND_CODE = 404;
+}
